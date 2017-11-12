b7d2c76b0b2b162df164d32e1a4bb6f35c34343f
ljy8509113 <31403803+ljy8509113@users.noreply.github.com>
2017-11-12 14:36:31 +0900

    ljy
    버전 수정

--- RecordingApp/app/src/main/java/play/and/eat/com/recording/SettingActivity.java
+++ RecordingApp/app/src/main/java/play/and/eat/com/recording/SettingActivity.java
@@ -1,21 +1,27 @@
 package play.and.eat.com.recording;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class SettingActivity extends Activity implements View.OnClickListener {
 
     Button _buttonBack;
     EditText _editIp;
-    RadioButton _radioTeacher;
-    RadioButton _radioStudent;
     RadioGroup _radioGroup;
     Button _buttonSave;
+
+    final String DATA_KEY = "save_data";
+    final String IS_TEACHER_KEY = "isTeacher";
+    final String IP_KEY = "ip";
+    SharedPreferences _pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,37 +32,53 @@ public class SettingActivity extends Activity implements View.OnClickListener {
         _editIp = (EditText)findViewById(R.id.edit_ip);
         _radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
         _buttonSave = (Button)findViewById(R.id.button_save);
-        _radioTeacher = (RadioButton)findViewById(R.id.radio_0);
-        _radioStudent = (RadioButton)findViewById(R.id.radio_1);
 
         _buttonBack.setOnClickListener(this);
         _buttonSave.setOnClickListener(this);
 
-//        _radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-//            public void onCheckedChanged(RadioGroup group, int checkedId) {
-//                // TODO Auto-generated method stub
-//                switch (checkedId) {
-//                    case R.id.radio_0:
-//                        //선생
-//                        break;
-//                    case R.id.radio_1:
-//                        //학생
-//                        break;
-//                }
-//            }
-//        });
+        _pref = getSharedPreferences(DATA_KEY, MODE_PRIVATE);
 
+        String ip = _pref.getString(IP_KEY, "");
+        boolean isTeacher = _pref.getBoolean(IS_TEACHER_KEY, false);
 
+        if(ip.equals("") == false){
+            _editIp.setText(ip);
 
+            if(isTeacher){
+                _radioGroup.check(R.id.radio_0);
+            }else{
+                _radioGroup.check(R.id.radio_1);
+            }
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.button_back:
-
+                onBackPressed();
                 break;
             case R.id.button_save :
+                 int selectedId = _radioGroup.getCheckedRadioButtonId();
+
+                 if(selectedId == R.id.radio_0){
+                     _pref.edit().putBoolean(IS_TEACHER_KEY, true);
+                 }else{
+                    _pref.edit().putBoolean(IS_TEACHER_KEY, false);
+                 }
+
+                String validIp = "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$";
+                String ip = _editIp.getText().toString();
+
+                if (!Pattern.matches(validIp, ip)) {
+                    Toast.makeText(this, "IP 확인이 필요합니다.", Toast.LENGTH_LONG).show();
+                    return;
+                }else{
+                    _pref.edit().putString(IP_KEY, ip);
+                    _pref.edit().commit();
+
+                    onBackPressed();
+                }
 
                 break;
         }
